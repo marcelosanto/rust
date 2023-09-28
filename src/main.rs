4881070702218e24220638ae85460b459e5b5541
Marcelo Santos <rocha.mer21@gmail.com>
2023-09-27 21:05:32 -0300

    fn mediana

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,22 @@ fn media(numeros: &Vec<i32>) -> f64 {
     soma as f64 / numeros.len() as f64
 }
 
-fn mediana(numeros: Vec<i32>) -> f64 {
-    3.0
+fn mediana(numeros: &Vec<i32>) -> f64 {
+    let mut numeros_sorted = numeros.clone();
+    numeros_sorted.sort();
+
+    let numero_meio = numeros.len() / 2;
+    if numeros_sorted.len() % 2 == 0 {
+        return media(&vec![
+            numeros_sorted[numero_meio - 1],
+            numeros_sorted[numero_meio],
+        ]) as f64;
+    }
+
+    return numeros_sorted[numero_meio] as f64;
 }
 
-fn moda(numeros: Vec<i32>) -> i32 {
+fn moda(_numeros: Vec<i32>) -> i32 {
     3
 }
 
@@ -20,4 +31,5 @@ fn main() {
     let numeros = vec![1, 1, 2, 3, 4, 4, 4, 5, 6, 0];
 
     println!("Media: {}", media(&numeros));
+    println!("Mediaana: {}", mediana(&numeros));
 }
