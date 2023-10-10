64839003cf9bf3453d75ddf9394f5dca9272bdf9
Marcelo Santos <rocha.mer21@gmail.com>
2023-10-09 21:16:06 -0300

    renomeada a pasta

--- src/exer/convert_to_pig_latin.rs
+++ src/exer/convert_to_pig_latin.rs
@@ -1,4 +1,4 @@
-fn convert_to_pig_latin(palavra: &String) -> String {
+pub fn convert_to_pig_latin(palavra: &String) -> String {
     let vogais = ["a", "e", "i", "o", "u"];
     let (primeira_letra, resto_palavra) = palavra.split_at(1);
     let primeira_letra_vogal = vogais.contains(&primeira_letra);
@@ -8,8 +8,4 @@ fn convert_to_pig_latin(palavra: &String) -> String {
     }
 
     return format!("{}-{}ay", resto_palavra, primeira_letra);
-}
-
-fn main() {
-    print!("{}", convert_to_pig_latin(&"tapple".to_string()));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,6 @@
-fn main() {}
+mod exer;
+
+use exer::convert_to_pig_latin::convert_to_pig_latin;
+fn main() {
+    println!("{}", convert_to_pig_latin(&"tapple".to_string()));
+}
