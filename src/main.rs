cb71cddaf1926fd1df287d12308773800d2c51de
Marcelo Santos <rocha.mer21@gmail.com>
2023-10-11 20:58:35 -0300

    Reto #40 - Rust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-mod exer;
+use std::io;
 
-use exer::convert_to_pig_latin::convert_to_pig_latin;
+/*
+ * Crea un programa que sea capaz de solicitarte un número y se
+ * encargue de imprimir su tabla de multiplicar entre el 1 y el 10.
+ * - Debe visualizarse qué operación se realiza y su resultado.
+ *   Ej: 1 x 1 = 1
+ *       1 x 2 = 2
+ *       1 x 3 = 3
+ *       ...
+ */
+
 fn main() {
-    println!("{}", convert_to_pig_latin(&"tapple".to_string()));
+    let mut numero = String::new();
+
+    println!("Digite um número: ");
+
+    io::stdin()
+        .read_line(&mut numero)
+        .expect("Erro ao ler o número");
+    let numero = numero.trim().parse::<i32>().unwrap();
+
+    for i in 1..=10 {
+        println!("{} x {} = {}", numero, i, numero * i);
+    }
 }
