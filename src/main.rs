77946144d55cdc6c4f4da1f8b1669356bac7b0cc
Marcelo Santos <rocha.mer21@gmail.com>
2023-10-15 17:07:18 -0300

    two sums

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+mod solution;
+
 fn main() {
-    
+    let slt = solution::Solution::two_sums([21, 2, 7, 15].to_vec(), 9);
+    println!("Solution: {:?}", slt);
 }
-
-
--- src/solution.rs
+++ src/solution.rs
@@ -1,5 +1,15 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn two_sums(nums: Vec<i32>, target: i32) -> Vec<i32> {}
+    pub fn two_sums(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for (i, n) in nums.iter().enumerate() {
+            for (j, m) in nums.iter().skip(i + 1).enumerate() {
+                if n + m == target {
+                    return vec![i as i32, (j + 1 + i) as i32];
+                }
+            }
+        }
+
+        vec![]
+    }
 }
